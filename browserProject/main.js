99bbcb160f8ad0eba0dff5a5e955a6e5e0c2f0d4
novandev <donovan.adams@students.makeschool.com>
2018-10-13 14:04:24 -0700

    commiting more to thre browser project

--- browserProject/main.js
+++ browserProject/main.js
@@ -6,41 +6,96 @@ window.onload= ()=> {
     else {
 
         var db;
+        var DBOpenRequest = window.indexedDB.open("ContactNames", 1);
+        DBOpenRequest.onsuccess = function(event) {
+            note.innerHTML += '<li>Database initialised.</li>';
 
-// Let us open our database
-        var DBOpenRequest = window.indexedDB.open("toDoList", 4);
-
-// these event handlers act on the database being opened.
-        DBOpenRequest.onerror = function (event) {
-            note.innerHTML += `<li>Error loading database.</li>
-                               <li>The error is ${event.error}</li>`;
-        };
-
-        DBOpenRequest.onsuccess = function (event) {
-            note.innerHTML += '<li>Database initialized.</li>';
-
-            // store the result of opening the database in the db
-            // variable. This is used a lot below
+            // store the result of opening the database in the db variable.
+            // This is used a lot below
             db = DBOpenRequest.result;
 
-            const transaction = db.transaction(["ContactNames"], "readwrite");
-
-            var objectStore = db.transaction("ContactNames").objectStore("ContactNames");
-
-            objectStore.openCursor().onsuccess = function(event) {
-                var cursor = event.target.result;
-                if (cursor) {
-                    alert("Name " + cursor.key + " is " + cursor.value);
-                    cursor.continue();
-                }
-                else {
-                    alert("No more entries!");
-                }
-            };
+            // Run the getData() function to get the data from the database
+            //
+            //ADD BUTTON SECTION
+            const addButton = document.querySelector('#add_button');
 
 
+            addButton.addEventListener('click', (event) =>{
+                event.preventDefault();       // prevents default submission
+                console.log(document.getElementById('nameAdd').value);
+                const transaction = db.transaction(["ContactNames"], "readwrite");
+                var objectStore = transaction.objectStore("ContactNames");
+                var request = objectStore.add(document.getElementById('nameAdd').value);
+                request.onsuccess = function(event) {
+                    console.log(event.target.result)
+                };
+
+            })
+            //
+            //END ADD BUTTON
+
+            //
+            //DELETE BUTTON SECTION
+            const deleteButton = document.querySelector('#delete_button');
+
+            deleteButton.addEventListener('click', (event) =>{
+                event.preventDefault();       // prevents default submission
+                var objectStore = db.transaction("ContactNames").objectStore("ContactNames");
+
+            })
+            //
+            //END DELETE SECTION
+
+            //
+            //SEARCH BUTTON
+            const searchButton = document.querySelector('#search_button');
+
+            searchButton.addEventListener('click', (event) =>{
+                event.preventDefault();       // prevents default submission
+                console.log(document.getElementById('name_query').value)
+
+                var objectStore = db.transaction("ContactNames").objectStore("ContactNames");
+
+                objectStore.openCursor().onsuccess = function(event) {
+                    var cursor = event.target.result;
+                    if (cursor.value == document.getElementById('name_query').value) {
+                        note.innerHTML += `<li>${cursor.value} found at position ${cursor.key} in the databse</li>`;
+                        cursor.continue();
+                    }
+                    else{
+                        cursor.continue();
+                    }
+
+                };
+
+            })
+            //
+            //END SEARCH BUTTON
 
         };
+
+
+
+
+
+
+
+
+
+            // objectStore.openCursor().onsuccess = function(event) {
+            //     var cursor = event.target.result;
+            //     if (cursor) {
+            //         alert("Name " + cursor.key + " is " + cursor.value);
+            //         cursor.continue();
+            //     }
+            //     else {
+            //         alert("No more entries!");
+            //     }
+            // };
+
+
+
+
 
 // This event handles the event whereby a new version of
 // the database needs to be created Either one has not
